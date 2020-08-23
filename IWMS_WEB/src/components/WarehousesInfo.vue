10c89d1e6122d3cbb83e089b2acec32225433832
wrp <1107344708@qq.com>
2020-08-23 09:41:33 +0800

    优化

--- IWMS_WEB/src/components/WarehousesInfo.vue
+++ IWMS_WEB/src/components/WarehousesInfo.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="Warehouses-Info">
     <!-- 温度卡片 -->
-    <div class="temperature-card">
+    <Tooltip :content="temperatureSettingStatus?'点击提交温度配置':'点击设置温度配置'" :always="temperatureSettingStatus">
+    <div class="temperature-card"
+         @click="settingTemperatureConfig">
       <!-- 实时温度信息 -->
       <div class="card-info">
         <div class="card-icon-box">
@@ -15,16 +17,18 @@
         </div>
       </div>
       <!-- 温度参数设置 -->
-      <div class="card-setting">
+      <div class="card-setting"  @click.stop>
         <div class="control-setting">
          <div class="setting-item">
             <div class="setting-item-title">监控报警</div>
             <i-switch :checked="realtimeTemperature.alarm"
+                      :disabled="!temperatureSettingStatus"
                       @on-change="changeTemperatureAlarm"></i-switch>
           </div>
           <div class="setting-item">
             <div class="setting-item-title">自动控制</div>
             <i-switch :checked="realtimeTemperature.automation"
+                      :disabled="!temperatureSettingStatus"
                       @on-change="changeTemperatureAutomation"></i-switch>
           </div>
         </div>
@@ -32,20 +36,22 @@
           <div class="setting-item">
             <div class="setting-item-title">最小值(℃)</div>
             <Input-number :max="60" :min="-20" :step="0.5" 
-                          @on-change="goUpdateTemperature"
+                          :disabled="!temperatureSettingStatus"
                           v-model="realtimeTemperature.min"></Input-number>
           </div>
           <div class="setting-item">
             <div class="setting-item-title">最大值(℃)</div>
-            <Input-number :max="60" :min="-20" :step="0.5" 
-                          @on-change="goUpdateTemperature"
+            <Input-number :max="60" :min="-20" :step="0.5"
+                          :disabled="!temperatureSettingStatus" 
                           v-model="realtimeTemperature.max"></Input-number>
           </div>
         </div>
       </div>
     </div>
+    </Tooltip>
     <!-- 湿度卡片 -->
-    <div class="humidity-card">
+    <Tooltip :content="humiditySettingStatus?'点击提交湿度配置':'点击设置湿度配置'" :always="humiditySettingStatus">
+    <div class="humidity-card" @click="settingHumidityConfig">
       <!-- 实时湿度信息 -->
       <div class="card-info">
         <div class="card-icon-box">
@@ -59,16 +65,18 @@
         </div>
       </div>
       <!-- 湿度参数设置 -->
-      <div class="card-setting">
+      <div class="card-setting" @click.stop>
         <div class="control-setting">
          <div class="setting-item">
             <div class="setting-item-title">监控报警</div>
             <i-switch :checked="realtimeHumidity.alarm"
+                      :disabled="!humiditySettingStatus"
                       @on-change="changeHumidityAlarm"></i-switch>
           </div>
           <div class="setting-item">
             <div class="setting-item-title">自动控制</div>
             <i-switch :checked="realtimeHumidity.automation"
+                      :disabled="!humiditySettingStatus"
                       @on-change="changeHumidityAutomation"></i-switch>
           </div>
         </div>
@@ -76,20 +84,22 @@
           <div class="setting-item">
             <div class="setting-item-title">最小值(％)</div>
             <Input-number :max="100" :min="0" :step="0.5" 
-                          @on-change="goUpdateHumidity"
+                          :disabled="!humiditySettingStatus"
                           v-model="realtimeHumidity.min"></Input-number>
           </div>
           <div class="setting-item">
             <div class="setting-item-title">最大值(％)</div>
             <Input-number :max="100" :min="0" :step="0.5" 
-                          @on-change="goUpdateHumidity"
+                          :disabled="!humiditySettingStatus"
                           v-model="realtimeHumidity.max"></Input-number>
           </div>
         </div>
       </div>
     </div>
+    </Tooltip>
     <!-- 光照度卡片 -->
-    <div class="illuminance-card">
+    <Tooltip :content="illuminanceSettingStatus?'点击提交光照配置':'点击设置光照配置'" :always="illuminanceSettingStatus">
+    <div class="illuminance-card" @click="settingIlluminanceConfig">
       <!-- 实时光照信息 -->
       <div class="card-info">
         <div class="card-icon-box">
@@ -103,16 +113,18 @@
         </div>
       </div>
       <!-- 光照参数设置 -->
-      <div class="card-setting">
+      <div class="card-setting" @click.stop>
         <div class="control-setting">
          <div class="setting-item">
             <div class="setting-item-title">监控报警</div>
             <i-switch :checked="realtimeIlluminance.alarm"
+                      :disabled="!illuminanceSettingStatus"
                       @on-change="changeIlluminanceAlarm"></i-switch>
           </div>
           <div class="setting-item">
             <div class="setting-item-title">自动控制</div>
             <i-switch :checked="realtimeIlluminance.automation"
+                      :disabled="!illuminanceSettingStatus"
                       @on-change="changeIlluminanceAutomation"></i-switch>
           </div>
         </div>
@@ -120,18 +132,19 @@
           <div class="setting-item">
             <div class="setting-item-title">最小值(％)</div>
             <Input-number :max="100" :min="0" :step="0.5" 
-                          @on-change="goUpdateIlluminance"
+                          :disabled="!illuminanceSettingStatus"
                           v-model="realtimeIlluminance.min"></Input-number>
           </div>
           <div class="setting-item">
             <div class="setting-item-title">最大值(％)</div>
             <Input-number :max="100" :min="0" :step="0.5" 
-                          @on-change="goUpdateIlluminance"
+                          :disabled="!illuminanceSettingStatus"
                           v-model="realtimeIlluminance.max"></Input-number>
           </div>
         </div>
       </div>
     </div>
+    </Tooltip>
 
     <!-- 报警状态 + 设备状态 -->
     <div class="alarm-card" :class="alarmMessage!='全部正常'?'is-alarm':''">
@@ -167,10 +180,10 @@ export default {
       humidityTimer   : null,
       illuminanceTimer: null,
       deviceTimer     : null,
-      //  延时消抖定时器
-      temperatureTimer2: null,
-      humidityTimer2   : null,
-      illuminanceTimer2: null,
+      //  属性设置状态
+      temperatureSettingStatus: 0,
+      humiditySettingStatus   : 0,
+      illuminanceSettingStatus: 0,
     }
   },
   methods: {
@@ -183,47 +196,47 @@ export default {
       reqSetHumidityConfig:   "reqSetHumidityConfig",    //请求 设置湿度参数 方法
       reqSetIlluminanceConfig:"reqSetIlluminanceConfig", //请求 设置光照参数 方法
     }),
-    changeTemperatureAlarm(val)      { this.realtimeTemperature.alarm = val;      this.goUpdateTemperature(); },
-    changeTemperatureAutomation(val) { this.realtimeTemperature.automation = val; this.goUpdateTemperature(); },
-    changeHumidityAlarm(val)         { this.realtimeHumidity.alarm = val;         this.goUpdateHumidity();    },
-    changeHumidityAutomation(val)    { this.realtimeHumidity.automation = val;    this.goUpdateHumidity();    },
-    changeIlluminanceAlarm(val)      { this.realtimeIlluminance.alarm = val;      this.goUpdateIlluminance(); },
-    changeIlluminanceAutomation(val) { this.realtimeIlluminance.automation = val; this.goUpdateIlluminance(); },
-    //  发送请求 更新仓库温度设置
-    goUpdateTemperature() {
-      //  500毫秒延时消抖
-      let timerLength = 500;  
-      if (this.temperatureTimer2) clearTimeout(this.temperatureTimer2);
-
-      //  延时500毫秒后发送请求
-      this.temperatureTimer2 = setTimeout(() => {
+    changeTemperatureAlarm(val)      { this.realtimeTemperature.alarm = val;       },
+    changeTemperatureAutomation(val) { this.realtimeTemperature.automation = val;  },
+    changeHumidityAlarm(val)         { this.realtimeHumidity.alarm = val;          },
+    changeHumidityAutomation(val)    { this.realtimeHumidity.automation = val;     },
+    changeIlluminanceAlarm(val)      { this.realtimeIlluminance.alarm = val;       },
+    changeIlluminanceAutomation(val) { this.realtimeIlluminance.automation = val;  },
+    //  设置温度属性配置
+    settingTemperatureConfig() {
+      //  开启设置状态
+      if (this.temperatureSettingStatus == 0) {
+        this.temperatureSettingStatus = 1;
+      }
+      //  提交设置,关闭设置状态
+      else {
         this.reqSetTemperatureConfig();
-        this.temperatureTimer2 = null;
-      }, timerLength);
+        this.temperatureSettingStatus = 0;
+      }
     },
-    //  发送请求 更新仓库湿度设置
-    goUpdateHumidity() {
-      //  500毫秒延时消抖
-      let timerLength = 500;
-      if (this.humidityTimer2) clearTimeout(this.humidityTimer2);
-
-      //  延时500毫秒后发送请求
-      this.humidityTimer2 = setTimeout(() => {
+    //  设置湿度属性配置
+    settingHumidityConfig() {
+      //  开启设置状态
+      if (this.humiditySettingStatus == 0) {
+        this.humiditySettingStatus = 1;
+      }
+      //  提交设置,关闭设置状态
+      else {
         this.reqSetHumidityConfig();
-        this.humidityTimer2 = null;
-      }, timerLength);
+        this.humiditySettingStatus = 0;
+      }
     },
-    //  发送请求 更新仓库光照设置
-    goUpdateIlluminance() {
-      //  500毫秒延时消抖
-      let timerLength = 500;
-      if (this.illuminanceTimer2) clearTimeout(this.illuminanceTimer2);
-
-      //  延时500毫秒后发送请求
-      this.illuminanceTimer2 = setTimeout(() => {
+    //  设置湿度属性配置
+    settingIlluminanceConfig() {
+      //  开启设置状态
+      if (this.illuminanceSettingStatus == 0) {
+        this.illuminanceSettingStatus = 1;
+      }
+      //  提交设置,关闭设置状态
+      else {
         this.reqSetIlluminanceConfig();
-        this.illuminanceTimer2 = null;
-      }, timerLength);
+        this.illuminanceSettingStatus = 0;
+      }
     },
   },
   computed: {
@@ -252,11 +265,20 @@ export default {
     // 3秒循环请求
     let timerLength = 1000 * 3;
     // 开启 请求实时温度信息 定时器
-    this.temperatureTimer = setInterval(this.reqRealtimeTemperature, timerLength);
+    this.temperatureTimer = setInterval(() => {
+      if (this.temperatureSettingStatus) return;
+      this.reqRealtimeTemperature();
+    }, timerLength);
     // 开启 请求实时湿度信息 定时器
-    this.humidityTimer    = setInterval(this.reqRealtimeHumidity, timerLength);
+    this.humidityTimer    = setInterval(() => {
+      if (this.humiditySettingStatus) return;
+      this.reqRealtimeHumidity();
+    }, timerLength);
     // 开启 请求实时光照信息 定时器
-    this.illuminanceTimer = setInterval(this.reqRealtimeIlluminance, timerLength);
+    this.illuminanceTimer = setInterval(() => {
+      if (this.illuminanceSettingStatus) return;
+      this.reqRealtimeIlluminance();
+    }, timerLength);
     // 开启 请求设备状态信息 定时器
     this.deviceTimer      = setInterval(this.reqDeviceStatus, timerLength);
   },
